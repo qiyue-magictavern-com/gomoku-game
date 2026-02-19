5d45342e0547e74e2d575a6b244aa4537db90aea
qiyue <qiyue@magictavern.com>
2026-02-19 11:26:33 +0800

    fix: 修复棋盘位置和缓存问题

--- server.js
+++ server.js
@@ -27,7 +27,12 @@ const server = http.createServer((req, res) => {
                 res.writeHead(500);
                 res.end('Error loading game');
             } else {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.writeHead(200, { 
+                    'Content-Type': 'text/html',
+                    'Cache-Control': 'no-cache, no-store, must-revalidate',
+                    'Pragma': 'no-cache',
+                    'Expires': '0'
+                });
                 res.end(data);
             }
         });
